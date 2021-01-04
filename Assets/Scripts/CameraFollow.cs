2d4f014251c4a380290c9e50bb007339e5556668
Avraam Yagudaev <avraamyagudaev@gmail.com>
2021-01-05 01:29:36 +0200

    Initial Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private float speed = 2.0f;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.position = player.transform.position + offset;
+       float interpolation = speed * Time.deltaTime;
+       Vector3 position = this.transform.position;
+       position.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, interpolation);
+       position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interpolation);
+       this.transform.position = position;
+    }
+}
---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 500.0f;
+
+    [SerializeField]
+    private float jumpForce = 6.0f;
+
+    private Rigidbody2D rigidBody;
+    private BoxCollider2D playerCollider;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        if (rigidBody == null)
+        {
+            Debug.LogError("Failed to start. Rigid body component not found!");
+        }
+
+        playerCollider = GetComponent<BoxCollider2D>();
+        if (playerCollider == null)
+        {
+            Debug.LogError("Failed to start. Collider component not found!");
+        }
+
+
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Failed to start. Animator component not found!");
+        }
+
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        Vector2 movement = new Vector2(deltaX, rigidBody.velocity.y);
+        rigidBody.velocity = movement;
+
+        Vector2 colliderCorner1 = new Vector2(playerCollider.bounds.max.x, playerCollider.bounds.min.y - 0.1f);
+        Vector2 colliderCorner2 = new Vector2(playerCollider.bounds.min.x, playerCollider.bounds.min.y - 0.2f);
+        Collider2D hit = Physics2D.OverlapArea(colliderCorner1, colliderCorner2);
+
+        bool isGrounded = (hit != null);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJumping", !isGrounded);
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        // Make animation react to movement
+        animator.SetFloat("horizontalSpeed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0.0f))
+        {
+            // Scale x to either positive or negative 1 to 'turn' the character
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1.0f, 1.0f);
+        }
+
+        //animator.SetBool("isJumping", !isGrounded);
+        animator.SetFloat("verticalSpeed", rigidBody.velocity.y);
+    }
+}
